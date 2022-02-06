13dc98bf1cb394ae36984484596ce7341a24b83a
Ball <piccomed@gmail.com>
2022-02-06 13:47:22 +0700

    Update SimpleNft_flat.sol

--- contract/SimpleNft_flat.sol
+++ contract/SimpleNft_flat.sol
@@ -1341,18 +1341,7 @@ contract NFT is ERC721Enumerable, Ownable {
   }
  
   function withdraw() public payable onlyOwner {
-    // This will pay HashLips 5% of the initial sale.
-    // You can remove this if you want, or keep it in to support HashLips and his channel.
-    // =============================================================================
-    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
-    require(hs);
-    // =============================================================================
-    
-    // This will payout the owner 95% of the contract balance.
-    // Do not remove this otherwise you will not be able to withdraw the funds.
-    // =============================================================================
-    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-    require(os);
-    // =============================================================================
+    (bool success, ) = payable(owner()).call{value: address(this).balance}("");
+    require(success);
   }
 }
